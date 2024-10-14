279e749dc69fc061e0adcf287e9e02d5f4edcad3
Stephan-W-H <stephanholtuin72@gmail.com>
2024-10-14 14:47:18 +0200

    Stephan Pushed
    mainView

---
+++ mainView/mainView.js
@@ -0,0 +1,12 @@
+app.innerHTML = /*HTML*/ `
+    <img src="helsevesen logo til prosjekt.png">
+    <div class="header">Home</div>
+    <div onclick="" class="headerBtn">Login</div>
+    <div onclick="" class="footer">Registrer deg her</div>
+    <div class="mainDiv">
+        Her kommer teksten for applikasjonen til helsevesnet...
+        
+    </div>
+
+`
+updateView();
