6615f6231869dfe806d72c41ddf2ded4f070f5b9
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-10-16 13:57:42 +0200

    Initi

--- login/loginView.js
+++ login/loginView.js
@@ -3,20 +3,22 @@ function loginView() {
     model.app.inView = /*HTML*/`
 
     <div class="loginContainer">
-        <div class="loginStyleS">Log in</div>
-        
-        <div class= inputStyle>
-            <label for="email">Email</label>
-            <input id="email" type="email" onchange="" placeholder="Email"> 
-        </div>
+        <div class="loginBox">
+            <h2>Log in</h2>
+            
+            <div class= "inputStyle">
+                <label for="email">Email</label>
+                <input id="email" type="email" onchange="" placeholder="Email"> 
+            </div>
 
-        <div>
-            <label for="pass">Password</label>
-            <input id="pass" type="password" onchange="" placeholder="Password"> 
+            <div class= "inputStyle">
+                <label for="pass">Password</label>
+                <input id="pass" type="password" onchange="" placeholder="Password"> 
+            </div>
+                
+            <button onclick="">Log in</button>
         </div>
-
     </div>
-        <button class="buttonStyle" onclick="">Log in</button>
     `;
     updateView();
 }
