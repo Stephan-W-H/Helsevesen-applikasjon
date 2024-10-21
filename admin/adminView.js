df41f66d7c736356b78e88eed4ec3979b2a868b2
Thomas Helland <thomas.helland97@gmail.com>
2024-10-21 11:07:03 +0200

    Progresjon på admin siden

--- admin/adminView.js
+++ admin/adminView.js
@@ -4,60 +4,76 @@
 //adminView();
 function adminView() {
     model.app.inView = `
-<div id="adminMain">
-    <form id="adminSearch">
-        <label for="search"></label>
-        <input id="search" placeholder="Søk her">
-    </form>
-    <br><br>
-    <div id="adminSearcResult" class="adminSearcResult" onclick="adminViewSecondPage()">
-        <div id="adminUsrInfo" class="adminUsrInfo">
+    <div id="adminMain">
+        <form id="adminSearch">
+            <label for="search"></label>
+            <input id="search" placeholder="Søk her">
+        </form>
+        <br><br>
         <div>
-            <img src="${model.data.users.usr[0].picture}" id="adminSearchImg" class="adminSearchImg">
-        </div>
-            <ul>
-                <li>Navn: ${model.data.users.usr[0].fname}</li>
-                <li>Kontakt info: ${model.data.users.usr[0].phone}</li>
-                <li>Sted: ${model.data.users.usr[0].place}</li>
-            </ul>
-        </div>
-        <hr>
-        <div id="adminUsrBio" class="adminUsrBio">
-            <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.<br>
-                Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.<br>
-                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.<br>
-                Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.<br>
-                <div>Status</div>
-            </p>
+            ${adminViewController()}
         </div>
     </div>
-    <br>
-    <div id="adminSearcResult" class="adminSearcResult" onclick="adminViewSecondPage()">
-<div>
-        <div id="adminUsrInfo" class="adminUsrInfo">
-        <div>
-            <img src="${model.data.users.usr[1].picture}" id="adminSearchImg" class="adminSearchImg">
-        </div>
-            <ul>
-                <li>Navn: ${model.data.users.usr[1].fname}</li>
-                <li>Kontakt info: ${model.data.users.usr[1].phone}</li>
-                <li>Sted: ${model.data.users.usr[1].place}</li>
-            </ul>
-        </div>
-</div>
-        <hr>
-        <div id="adminUsrBio" class="adminUsrBio">
-            <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.<br>
-                Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.<br>
-                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.<br>
-                Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.<br>
-                <div>Status</div>
-            </p>
-        </div>
-    </div>
-</div>
 `;
+
     updateView();
 }
+// function adminView() {
+//     model.app.inView = `
+// <div id="adminMain">
+//     <form id="adminSearch">
+//         <label for="search"></label>
+//         <input id="search" placeholder="Søk her">
+//     </form>
+//     <br><br>
+//     <div id="adminSearcResult" class="adminSearcResult" onclick="adminViewSecondPage()">
+//         <div id="adminUsrInfo" class="adminUsrInfo">
+//         <div>
+//             <img src="${model.data.users.usr[0].picture}" id="adminSearchImg" class="adminSearchImg">
+//         </div>
+//             <ul>
+//                 <li>Navn: ${model.data.users.usr[0].fname}</li>
+//                 <li>Kontakt info: ${model.data.users.usr[0].phone}</li>
+//                 <li>Sted: ${model.data.users.usr[0].place}</li>
+//             </ul>
+//         </div>
+//         <hr>
+//         <div id="adminUsrBio" class="adminUsrBio">
+//             <p>
+//                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.<br>
+//                 Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.<br>
+//                 Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.<br>
+//                 Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.<br>
+//                 <div>Status</div>
+//             </p>
+//         </div>
+//     </div>
+//     <br>
+//     <div id="adminSearcResult" class="adminSearcResult" onclick="adminViewSecondPage()">
+// <div>
+//         <div id="adminUsrInfo" class="adminUsrInfo">
+//         <div>
+//             <img src="${model.data.users.usr[1].picture}" id="adminSearchImg" class="adminSearchImg">
+//         </div>
+//             <ul>
+//                 <li>Navn: ${model.data.users.usr[1].fname}</li>
+//                 <li>Kontakt info: ${model.data.users.usr[1].phone}</li>
+//                 <li>Sted: ${model.data.users.usr[1].place}</li>
+//             </ul>
+//         </div>
+// </div>
+//         <hr>
+//         <div id="adminUsrBio" class="adminUsrBio">
+//             <p>
+//                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.<br>
+//                 Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.<br>
+//                 Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.<br>
+//                 Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.<br>
+//                 <div>Status</div>
+//             </p>
+//         </div>
+//     </div>
+// </div>
+// `;
+//     updateView();
+// }
--- admin/adminViewController.js
+++ admin/adminViewController.js
@@ -1,4 +1,26 @@
 //
 //
 //
-function adminViewController() {}
+function adminViewController() {
+    let html;
+    for (let i = 0; i < model.data.users.usr.length; i++) {
+        html += `
+        <div>
+            <div>
+                <img src="${model.data.users.usr[i].picture}">
+                <ul>
+                    <li>Navn: ${model.data.users.usr[i].fname}</li>
+                    <li>Telefon: ${model.data.users.usr[i].phone}</li>
+                    <li>Setd: ${model.data.users.usr[i].place}</li>
+                </ul>
+            </div>
+            <hr>
+            <div>
+                <p>Info: ${model.data.users.usr[i].info}</p>
+                <p>Status: ${model.data.users.usr[i].mission}</p>
+            </div>
+        </div>
+        `;
+        return html;
+    }
+}
