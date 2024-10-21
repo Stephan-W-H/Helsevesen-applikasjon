efaaba805101040e40dfc45307eee23baa7c2951
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2024-10-21 15:02:46 +0200

    kiim push

--- creatAccount/creatAccountView.js
+++ creatAccount/creatAccountView.js
@@ -1,17 +1,18 @@
 function creatAccountView() {
+    let p = model.input.registration
     model.app.inView = /*HTML*/ 
     `
     <div class="topText">Registrering</div>
     <div class="accountRegBox">
     <div class="containerDiv">
     <label for="firstName">Fornavn:</label>
-    <input id="firstName" type="text" onchange="">
+    <input id="firstName" type="text" onchange="p.firstname = this.value">
 
     <label for="lastName">Etternavn:</label>
-    <input id="lastName" type="text" onchange="">
+    <input id="lastName" type="text" onchange="p.lastname = this.value">
 
     <label for="gender">Kjønn:</label>
-    <select id="gender" onchange="">
+    <select id="gender" onchange="p.gender = this.value">
         <option value="Dame">Dame</option>
         <option value="Mann">Mann</option>
         <option value="Ikke-Binær">Ikke-Binær</option>
@@ -19,24 +20,25 @@ function creatAccountView() {
     
 
     <label for="E-mail">Epost:</label>
-    <input id="E-mail" type="email" placeholder="abc@abc.com" onchange="getInputEmail(this.value)"><br>
-
+    <input id="E-mail" type="email" placeholder="abc@abc.com" onchange="p.email = this.value"><br>
+    <label for="password">Epost:</label>
+    <input id="password" type="password" placeholder="********" onchange="p.password = this.value"><br>
     <label for="city">By:</label>
-    <input id="city" type="text" onchange="">
+    <input id="city" type="text" onchange="p.place = this.value">
 
     <label for="number">Telefonnummer:</label>
-    <input id="number" type="tel" onchange="">
+    <input id="number" type="tel" onchange="p.phone = this.value">
 
     <label for="age">Alder:</label>
-    <input id="age" type="text" onchange="">
+    <input id="age" type="text" onchange="p.age = this.value">
 
     <label for="ethnicity">Etnisitet:</label>
-    <input id="ethnicity" type="text" onchange=""><br>
+    <input id="ethnicity" type="text" onchange="p.ethnicity = this.value"><br>
     </div>
 
     <div class="containerDiv">
     <label for="licens">Førerkort Klasser:</label>
-    <select id="licens" onchange="">
+    <select id="licens" onchange="p.driverLicence = this.value">
     <option value="M"> M Moped</option>
     <option value="A1">A1 lett motorsykkel</option>
     <option value="A">A Motorsykkel</option>
@@ -48,7 +50,7 @@ function creatAccountView() {
     </select>
 
     <label for="hobby">Hobbyer:</label>
-    <select id="hobby" onchange="">
+    <select id="hobby" onchange="p.interest = this.value">
     <option></option>
     ${creatHobbyOptions()}
     </select>
@@ -56,19 +58,19 @@ function creatAccountView() {
     <input id="hobbyinput" type="text" onchange="addHobby(this.value)">
 
     <label for="education">Utdanning/Kurs</label>
-    <input id="education" type="text" onchange=""><br>
+    <input id="education" type="text" onchange="p.exp = this.value"><br>
 
     <label for="availebilety">Tilgjengelighet</label>
     <input id="availebilety" type="date" onchange="">
 
     <label for="about">Om Deg:</label>
-    <input id="about" type="text" onchange="">
+    <input id="about" type="text" onchange="p.info = this.value">
 
     <label for="pic">Link til bilde:</label>
-    <input id="pic" type="text" onchange="">
+    <input id="pic" type="text" onchange="p.picture = this.value">
 
     <label for="travelTime">Reisetid:</label>
-    <input id="travelTime" type="number" placeholder="hele timer" onchange=""><br>
+    <input id="travelTime" type="number" placeholder="hele timer" onchange="p.travel = this.value"><br>
     </div>
 </div>
 
--- creatAccount/creatAccountViewController.js
+++ creatAccount/creatAccountViewController.js
@@ -12,7 +12,10 @@ function addHobby(hobby)
     if(model.data.users.userInterests.includes(hobby) == false)
         {
             model.data.users.userInterests.push(hobby)
+            model.input.registration.interest = hobby
             console.log(model.data.users.userInterests)
+            console.log(model.input.registration.interest)
+
         }
         else{alert('Din hobby eksisterer i oversikt listen, venligst huk av for ønskede hobbyer i listen først')}
 }
@@ -24,70 +27,17 @@ function autogenId()
 }
 
 
-function getInputFirstName(input)
-{
-    model.input.registration.firstname = input
-}
-function getInputLastName(input)
-{
-    model.input.registration.lastname = input
-}
-function getInputEmail(input)
-{
-    model.input.registration.email = input
-}
-function getInputPassword(input)
-{
-    model.input.registration.password = input
-}
-function getInputPhone(input)
-{
-    model.input.registration.phone = input
-}
-function getInputAge(input)
-{
-    model.input.registration.age = input
-}
-function getInputGender(input)
-{
-    model.input.registration.gender = input
-}
-function getInputEthnicity(input)
-{
-    model.input.registration.ethnicity = input
-}
-function getInputLicens(input)
-{
-    model.input.registration.driverLicence = input
-}
-function getInputPlace(input)
-{
-    model.input.registration.place = input
-}
-function getInputInterest(input)
-{
-    model.input.registration.interest = input
-}
-function getInputExp(input)
-{
-    model.input.registration.exp = input
-}
+
+
+
+
+
 function getInputTime(input)
 {
     model.input.registration.time = input
 }
-function getInputTravel(input)
-{
-    model.input.registration.travel = input
-}
-function getInputInfo(input)
-{
-    model.input.registration.info = input
-}
-function getInputPic(input)
-{
-    model.input.registration.picture = input
-}
+
+
 
 function regisetr() 
 {
