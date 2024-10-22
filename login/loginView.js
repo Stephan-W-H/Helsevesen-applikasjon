c3be95e0e0c02e1256e6ccc3c11b7addf0ac7e8b
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-10-22 14:40:09 +0200

    Initi

--- login/loginView.js
+++ login/loginView.js
@@ -9,12 +9,12 @@ function loginView() {
                 
                 <div class= "formGroup">
                     <label for="email">Email</label>
-                    <input id="email" type="email" class="inputField" onchange=""> 
+                    <input id="email" type="email" class="inputField" onchange="model.input.login.username = this.value"> 
                 </div>
 
                 <div class= "formGroup">
                     <label for="pass">Password</label>
-                    <input id="pass" type="password" class="inputField" onchange=""> 
+                    <input id="pass" type="password" class="inputField" onchange="model.input.login.password = this.value"> 
                 </div>
                     
                 <button class="btn" onclick="loginValidation()">Log in</button>
