c8ade57ca2d818f2d1deefadb96ba07affa4f184
Thomas Helland <thomas.helland97@gmail.com>
2024-10-23 10:20:58 +0200

    aa

--- admin/adminNoteController.js
+++ admin/adminNoteController.js
@@ -4,6 +4,5 @@
 function adminNoteController(x) {
     //const noteId = document.getElementById("adminNote").value;
     console.log(x);
-    let y = model.data.users.adminSeconPage.note.noteText.push(x);
-    return y;
+    model.data.users.adminSeconPage.note.noteText.push(x);
 }
