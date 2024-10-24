73fc327648e28590c0e47a82bf3637f9612ff92f
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2024-10-24 08:59:27 +0200

    kim

--- creatAccount/creatAccountView.js
+++ creatAccount/creatAccountView.js
@@ -6,10 +6,10 @@ function creatAccountView() {
     <div class="accountRegBox">
     <div class="containerDiv">
     <label for="firstName">Fornavn:</label>
-    <input id="firstName" type="text" onchange="p.firstname = this.value">
+    <input id="firstName" type="text" onchange="model.input.registration.firstname = this.value">
 
     <label for="lastName">Etternavn:</label>
-    <input id="lastName" type="text" onchange="p.lastname = this.value">
+    <input id="lastName" type="text" onchange="model.input.registration.lastname = this.value">
 
     <label for="gender">Kjønn:</label>
     <select id="gender" onchange="p.gender = this.value">
@@ -20,25 +20,25 @@ function creatAccountView() {
     
 
     <label for="E-mail">Epost:</label>
-    <input id="E-mail" type="email" placeholder="abc@abc.com" onchange="p.email = this.value"><br>
+    <input id="E-mail" type="email" placeholder="abc@abc.com" onchange="model.input.registration.email"><br>
     <label for="password">Epost:</label>
-    <input id="password" type="password" placeholder="********" onchange="p.password = this.value"><br>
+    <input id="password" type="password" placeholder="********" onchange="model.input.registration.password = this.value"><br>
     <label for="city">By:</label>
-    <input id="city" type="text" onchange="p.place = this.value">
+    <input id="city" type="text" onchange="model.input.registration.place = this.value">
 
     <label for="number">Telefonnummer:</label>
-    <input id="number" type="tel" onchange="p.phone = this.value">
+    <input id="number" type="tel" onchange="model.input.registration.phone = this.value">
 
     <label for="age">Alder:</label>
-    <input id="age" type="text" onchange="p.age = this.value">
+    <input id="age" type="text" onchange="model.input.registration.age = this.value">
 
     <label for="ethnicity">Etnisitet:</label>
-    <input id="ethnicity" type="text" onchange="p.ethnicity = this.value"><br>
+    <input id="ethnicity" type="text" onchange="model.input.registration.ethnicity = this.value"><br>
     </div>
 
     <div class="containerDiv">
     <label for="licens">Førerkort Klasser:</label>
-    <select id="licens" onchange="p.driverLicence = this.value">
+    <select id="licens" onchange="model.input.registration.driverLicence = this.value">
     <option value="M"> M Moped</option>
     <option value="A1">A1 lett motorsykkel</option>
     <option value="A">A Motorsykkel</option>
@@ -50,7 +50,7 @@ function creatAccountView() {
     </select>
 
     <label for="hobby">Hobbyer:</label>
-    <select id="hobby" onchange="p.interest = this.value">
+    <select id="hobby" onchange="$model.input.registration.interest = this.value">
     <option></option>
     ${creatHobbyOptions()}
     </select>
@@ -58,19 +58,30 @@ function creatAccountView() {
     <input id="hobbyinput" type="text" onchange="addHobby(this.value)">
 
     <label for="education">Utdanning/Kurs</label>
-    <input id="education" type="text" onchange="p.exp = this.value"><br>
+    <input id="education" type="text" onchange="model.input.registration.exp = this.value"><br>
 
     <label for="availebilety">Tilgjengelighet</label>
-    <input id="availebilety" type="date" onchange="">
+    <label id="availebilety" for="From">Fra</label>
+    <input id="From" type="date" onchange="model.input.registration.date.startTime = this.value">
+    <label for="To">Til</label>
+    <input id="To" type="date" onchange="model.input.registration.date.stoptime = this.value">
+    <input id="availebilety" type="checkbox"  onchange="model.input.registration.date.days.sunday = 'Søndag'">
+    <input id="availebilety" type="checkbox"  onchange="model.input.registration.date.days.saturday = 'Lørdag'">
+    <input id="availebilety" type="checkbox"  onchange="model.input.registration.date.days.friday = 'Fredag'">
+    <input id="availebilety" type="checkbox"  onchange="model.input.registration.date.days.thursday = 'Torsdag'">
+    <input id="availebilety" type="checkbox"  onchange="model.input.registration.date.days.wedensday = 'Onsdag'">
+    <input id="availebilety" type="checkbox"  onchange="model.input.registration.date.days.thusday = 'Tirsdag'">
+    <input id="availebilety" type="checkbox"  onchange="model.input.registration.date.days.monday = 'Mandag'">
+    
 
     <label for="about">Om Deg:</label>
-    <input id="about" type="text" onchange="p.info = this.value">
+    <input id="about" type="text" onchange="model.input.registration.info = this.value">
 
     <label for="pic">Link til bilde:</label>
-    <input id="pic" type="text" onchange="p.picture = this.value">
+    <input id="pic" type="text" onchange="model.input.registration.picture = this.value">
 
     <label for="travelTime">Reisetid:</label>
-    <input id="travelTime" type="number" placeholder="hele timer" onchange="p.travel = this.value"><br>
+    <input id="travelTime" type="number" placeholder="hele timer" onchange="model.input.registration.travel = this.value"><br>
     </div>
 </div>
 
--- creatAccount/creatAccountViewController.js
+++ creatAccount/creatAccountViewController.js
@@ -20,32 +20,58 @@ function addHobby(hobby)
         else{alert('Din hobby eksisterer i oversikt listen, venligst huk av for ønskede hobbyer i listen først')}
 }
 
-function autogenId()
-{
-    model.input.registration.id = model.data.users.usr.length + 1
-    console.log(model.input.registration.id)
-}
-
-
-
-
-
-
-
-function getInputTime(input)
-{
-    model.input.registration.time = input
-}
 
 
 
 function regisetr() 
 {
     let p = model.input.registration
+    let newUser = {
+            id: model.data.users.usr.length + 1,
+            firstname: p.firstname,
+            lastname: p.lastname,
+            email: p.email,
+            password: p.password,
+            phone: p.phone,
+            age: p.age,
+            gender: p.gender,
+            ethnicity: p.ethnicity,
+            driverLicence: p.driverLicence,
+            place: p.place,
+            interest: p.interest,
+            exp: p.exp,
+            date: {
+                startTime: p.date.startTime,
+                stoptime: p.date.stoptime,
+                days: p.date.days
+            },
+            travel: p.travel,
+            info: p.info,
+            picture: p.picture,
+    }  
 
-
-    model.data.users.usr.push(p)
-
-    p = null
+    model.data.users.usr.push(newUser)
+    console.log( model.data.users.usr)
+    p.id = null
+    p.firstname = null
+    p.lastname = null
+    p.email = null
+    p.password = null
+    p.phone = null
+    p.age = null
+    p.gender = null
+    p.ethnicity = null
+    p.driverLicence = null
+    p.place = null
+    p.interest = null
+    p.exp = null
+    p.date = {
+        startTime: '',
+        stoptime: '',
+        days: [],
+    },
+    p.travel = null
+    p.info = null
+    p.picture = null
     
 }
