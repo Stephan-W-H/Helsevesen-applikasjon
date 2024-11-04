acbcceb22d7342c0971ab69530892e767a8bfae2
Stephan-W-H <stephanholtuin72@gmail.com>
2024-11-04 13:17:25 +0100

    css og profileViewController

--- profile/profileView.js
+++ profile/profileView.js
@@ -1,7 +1,40 @@
 function profileView() {
-    //model.app.pages = "userView";
+    model.app.pages = "userView";
     model.app.inView = /*HTML*/ `
     <div>${wprofileView()}</div>
     `;
+    leftSideMenu();
+    profileHeader();
+    profileFooter();
     updateView();
 }
+
+function leftSideMenu(){ 
+    model.app.leftSideMenu = /*html*/`
+    <img class="">
+    
+    <div class="items">Kontakt info</div>
+    <div class="items">Kvalifikiasjoner</div>
+    <div class="items">Sertifiseringer</div>
+    <div class="items">Bio</div>
+    <div class="items">Rediger</div>
+    <div class="items">Oppdrag</div>
+    </div>
+    `;
+    
+}
+
+
+function profileHeader(){
+    model.app.header = /*html*/ `
+    <div>Din innsats teller! Takk for at du gir av din tid til de som trenger det mest.</div>
+    `;
+}
+
+function profileFooter(){
+    model.app.footer = /*html*/ `
+    <div>Denne plattformen støtter eldreomsorg gjennom frivillig innsats. Sammen kan vi nå flere.</div>
+    `;
+}
+
+        
--- profile/profileViewController.js
+++ profile/profileViewController.js
@@ -1,37 +1,60 @@
-function wprofileView() {
+function wprofileView(){
     let html = "";
     for (let i = 0; i < model.data.users.usr.length; i++) {
-        if (model.app.usrId === model.data.users.usr[i].id) {
+        if (model.app.usrId == model.data.users.usr[i].id) {
             let p = model.data.users.usr[i];
 
-            html = /*HTML*/ `
-            <div class="header">
-            <div>${p.fname}</div>
-            <div>${p.lname}</div>
-        </div>
-    
-        <div class="container">
-        <div class="item1">Kontakt info</div>
-        <div class="item2">Kvalifikiasjoner</div>
-        <div class="item3">Sertifiseringer</div>
-        <div class="item4">Bio</div>
-        <div class="item5">Rediger</div>
-        <div class="item6"></div>
+        html = /*HTML*/ `
+        <div class="header">
+        <div>${p.fname}</div>
+        <div>${p.lname}</div>
         </div>
     
         <div class="bioText">
         <div>Her kommer Bio og Info</div>
         ${p.info}
         </div>
-    
+
         <div class="foot">
         <div>Tilgjengelighet</div>
         ${p.date}
         </div>
-            `;
         
-        }
+    `
+    console.log(model.app.usrId)
+    console.log(html)
+    }
+}
+return html;
+}
+    
+function kontaktInfo(){
+    const user = model.data.users.usr
+    
+    const idOfUsr = model.app.usrId;
+    const index = model.data.users.usr.findIndex((usr) => usr.id === idOfUsr);
+    
+    if (index !== -1) {
+        <div>
+        <div>${model.data.users.usr[index].fname}</div>
+        <div>${model.data.users.usr[index].lname}</div>
+        <div>${model.data.users.usr[index].gender}</div>
+        <div>${model.data.users.usr[index].email}</div>
+        <div>${model.data.users.usr[index].phone}</div>
+        <div>${model.data.users.usr[index].age}</div>
+        <div>${model.data.users.usr[index].ethnicity}</div>
+        <div>${model.data.users.usr[index].driverLicence}</div>
+        <div>${model.data.users.usr[index].place}</div>
+        <div>${model.data.users.usr[index].interest}</div>
+        <div>${model.data.users.usr[index].exp}</div>
+        <div>${model.data.users.usr[index].travel}</div>
+        <div>${model.data.users.usr[index].info}</div>
+        <div>${model.data.users.usr[index].picture}</div>
+        <div>${model.data.users.usr[index].mission}</div>
+
+        </div>
+
+
 
     }
-    return html;
 }
