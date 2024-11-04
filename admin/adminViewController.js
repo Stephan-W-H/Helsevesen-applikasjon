45711fe81fbb527c79f91fa7bf4d48d9f1241bec
Thomas Helland <thomas.helland97@gmail.com>
2024-11-04 14:25:14 +0100

    Fixed notes

--- admin/adminViewController.js
+++ admin/adminViewController.js
@@ -3,7 +3,17 @@
 //
 
 function adminStoreUsrData(storedData) {
-    model.data.users.adminSeconPage = model.data.users.usr[storedData];
+    // Store usr data in adminSeconPage (temp storage)
+    model.data.users.adminSeconPage = {
+        ...model.data.users.usr[storedData], // ... aka spread operator to copy form one array to another
+        note: {
+            noteId: model.data.users.usr[storedData].note?.noteId || [],
+            adminUsername:
+                model.data.users.usr[storedData].note?.adminUsername || [],
+            noteText: model.data.users.usr[storedData].note?.noteText || [],
+        },
+    };
+
     model.input.adminInPut.selectedUsrId = model.data.users.adminSeconPage.id;
     model.app.page = "adminSecondPage";
     pageSwitch();
